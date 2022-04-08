8ffb926d03b0bd1d2cad81c52f3e06f5f2f5f37d
Jiboxiake <jiboxiake@gmail.com>
2022-04-08 15:03:49 -0400

    use list iterator to save space

--- src/dedp/DistanceOracles/Precomputation/DiameterComputationThread.java
+++ src/dedp/DistanceOracles/Precomputation/DiameterComputationThread.java
@@ -4,15 +4,23 @@ import dedp.indexes.edgedisjoint.ConnectedComponent;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.ListIterator;
 
 public class DiameterComputationThread extends Thread {
     public ArrayList<DiameterQueryEntry> workloads = new ArrayList<>();
     @Override
     public void run(){
         DiameterQueryEntry entry = null;
-        for(int i=0; i<workloads.size();i++){
+     /*  for(int i=0; i<workloads.size();i++){
             entry = workloads.get(i);
             entry.computation();
+        }*/
+        //use list iterator to save space
+        ListIterator<DiameterQueryEntry>iter = workloads.listIterator();
+        while(iter.hasNext()){
+            entry = iter.next();
+            entry.computation();
+            iter.remove();
         }
     }
 }
